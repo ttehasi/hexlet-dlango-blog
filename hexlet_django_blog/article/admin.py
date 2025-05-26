d7c0e358b0feb9cf528e9d1ff15e5c4e43439a4e
ttehasi <tatiana.chudowa@my.com>
2025-05-26 09:46:23 +0300

    django admin

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Article
+from django.contrib.admin import DateFieldListFilter
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'created_at'
+    )
+    search_fields = ["name", "body"]
+    list_filter = (
+        ('created_at', DateFieldListFilter),
+    )
