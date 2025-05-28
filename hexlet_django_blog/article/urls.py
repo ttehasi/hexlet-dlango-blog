f58e74d20f543edc0000e48463913e08f3679ed5
ttehasi <tatiana.chudowa@my.com>
2025-05-28 20:09:38 +0300

    add create from CRUD

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from hexlet_django_blog.article.views import IndexView, ArticleView
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
 
 urlpatterns = [
-    path("", IndexView.as_view()),
-    path('<int:id>/', ArticleView.as_view(), name='article_id')
+    path("", IndexView.as_view(), name='article_index'),
+    path('<int:id>/', ArticleView.as_view(), name='article_id'),
+    path('create/', ArticleFormCreateView.as_view(), name='article_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
+from django.contrib import messages
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
+        mess = messages.get_messages(request)
         return render(
             request,
             'articles/index.html',
             context={
                 'articles':articles,
+                'messages': mess,
                 },
         )
         
@@ -25,3 +30,20 @@ class ArticleView(View):
                 'article': article,
             },
         )
+        
+        
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(
+            request, 'articles/create.html', {'form': form}
+        )
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Статья успешно создана')
+            return redirect(reverse('article_index'))
+        messages.add_message(request, messages.ERROR, 'Ошибки')
+        return render(request, 'articles/create.html', {'form': form})
